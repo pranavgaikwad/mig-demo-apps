24c7c232327c99c83514b9e5eaa750ec113fe107
Pranav Gaikwad <pgaikwad@redhat.com>
2026-01-28 10:29:46 -0500

    never open the html test report
    Signed-off-by: Pranav Gaikwad <pgaikwad@redhat.com>

--- kai/nodejs/todo-app/playwright.config.ts
+++ kai/nodejs/todo-app/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ['html'],
+    ['html', { open: 'never' }],
     ['json', { outputFile: 'test-results/results.json' }],
     ['junit', { outputFile: 'test-results/junit.xml' }],
   ],
